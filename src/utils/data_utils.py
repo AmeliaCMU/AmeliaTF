1336dc309fc872166aaa72946298c6672a9ec045
alonsocanov <45244628+alonsocanov@users.noreply.github.com>
2024-08-08 19:29:42 -0400

    remove csv

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -19,8 +19,8 @@ pd.options.mode.chained_assignment = None
 
 
 def get_filtered_list(airport, base_dir, file_list, min_agents, max_agents):
-    data_dirs = [os.path.join(base_dir, f).replace('.csv', '') for f in file_list if airport in f]
-    breakpoint()
+    data_dirs = [os.path.join(base_dir, f) for f in file_list if airport in f]
+    # breakpoint()
     data_files = [os.path.join(dir_, f)
                   for dir_ in data_dirs for f in os.listdir(dir_)]
     # Each pickle file has a number at the end indicating the number of agents in the scenario.
