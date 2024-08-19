b1fa017a09288438e3059a3d5d1220dd7e665483
alonsocanov <45244628+alonsocanov@users.noreply.github.com>
2024-08-19 18:08:55 -0400

    src

--- src/models/components/amelia.py
+++ src/models/components/amelia.py
@@ -9,29 +9,31 @@ from src.models.components.cross_attention import CrossAttentionBlock
 from src.models.components.gmm import GMM
 from src.models.components.common import MLP, LayerNorm
 
+
 class AmeliaTF(nn.Module):
-    """ Context-aware model for trajectory prediction on airport data. Baseline designed for both, 
-    trajectory and context data. Largely based on the SceneTransformer: 
+    """ Context-aware model for trajectory prediction on airport data. Baseline designed for both,
+    trajectory and context data. Largely based on the SceneTransformer:
     https://arxiv.org/pdf/2106.08417.pdf """
+
     def __init__(self, config: EasyDict) -> None:
         super().__init__()
 
-        self.encoder_config = config.encoder 
-        self.decoder_config = config.decoder 
-        
+        self.encoder_config = config.encoder
+        self.decoder_config = config.decoder
+
         self.in_size = self.encoder_config.in_size + self.encoder_config.interp_flag
         self.embed_size = self.encoder_config.embed_size
 
-        # Agent feature extraction 
+        # Agent feature extraction
         self.agents_fe = nn.Sequential(*[
             nn.Linear(self.in_size, self.embed_size),
             LayerNorm(self.embed_size, self.encoder_config.bias),
             nn.ReLU(),
             nn.Linear(self.embed_size, self.embed_size)
         ])
-        
-        # Context feature extraction 
-        # TODO: once the best configuration is found, remove this mess. 
+
+        # Context feature extraction
+        # TODO: once the best configuration is found, remove this mess.
         ctx_enc_type = self.encoder_config.context_encoder_type
         if ctx_enc_type == 'v0':
             from src.models.components.context import ContextNetv0 as ContextNet
@@ -66,7 +68,7 @@ class AmeliaTF(nn.Module):
         for _ in range(self.encoder_config.num_satt_pre_blocks):
             self.att_blocks.append(SelfAttentionBlock(self.encoder_config, across='time'))
             self.att_blocks.append(SelfAttentionBlock(self.encoder_config, across='agents'))
-    
+
         # Cross-attention block for agents and map
         for _ in range(self.encoder_config.num_catt_pre_blocks):
             self.att_blocks.append(CrossAttentionBlock(self.encoder_config))
@@ -78,7 +80,7 @@ class AmeliaTF(nn.Module):
 
         for _ in range(self.encoder_config.num_catt_blocks):
             self.att_blocks.append(CrossAttentionBlock(self.encoder_config))
-        
+
         # Post self-attention blocks
         for _ in range(self.encoder_config.num_satt_post_blocks):
             self.att_blocks.append(SelfAttentionBlock(self.encoder_config, across='time'))
@@ -86,21 +88,21 @@ class AmeliaTF(nn.Module):
         self.att_blocks = nn.ModuleList(self.att_blocks)
 
         self.refine_mlp = MLP(self.encoder_config)
-        
+
         # Finally, trajectory decoding is done using Gaussian Mixtures
         self.decoder_config.in_size = self.embed_size
         self.decoder_head = GMM(self.decoder_config)
-    
+
         self.apply(self._init_weights)
         print("number of parameters: %.2fM" % (self.get_num_params()/1e6,))
-    
+
     @property
     def num_dec_heads(self) -> int:
         return self.decoder_head.num_futures
 
     def get_num_params(self, non_embedding: bool = True) -> int:
-        """ Returns the number of parameters in the model. For non-embedding count (default), the 
-        position embeddings get subtracted. The token embeddings would too, except due to the parameter 
+        """ Returns the number of parameters in the model. For non-embedding count (default), the
+        position embeddings get subtracted. The token embeddings would too, except due to the parameter
         sharing these params are actually used as weights in the final layer, so we include them. """
         n_params = sum(p.numel() for p in self.parameters())
         if non_embedding:
@@ -117,42 +119,42 @@ class AmeliaTF(nn.Module):
             torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
 
     def forward(self, x: torch.tensor, **kwargs) -> Tuple:
-        """ Model's forward module. 
-        
+        """ Model's forward module.
+
         Inputs
         ------
             x[torch.tensor(B, A, T, D)]: input tensor containing the trajectory information.
                 B: batch size
                 A: number of agents
                 T: trajectory length
-                D: number of input dimensions. 
-            kwargs[Any]: other keyword arguments. 
+                D: number of input dimensions.
+            kwargs[Any]: other keyword arguments.
                 Should contain a key 'context' containing the map information in vectorized format.
                 c[torch.tensor(B, T, P, Dc)]: is the tensor containing the context information
                     P: number of polylines
-                    Dc: number of input dimensions of the context. 
+                    Dc: number of input dimensions of the context.
 
         Outputs
         -------
             pred_scores[torch.tensor(B, A, T, H)]: prediction scores for each prediction trajectory's
-                prediction head. 
-                H: number of predicted heads. 
-            mu[torch.tensor(B, A, T, H, D)]: predicted means for each trajectory. 
-            sigma[torch.tensor(B, A, T, H, D)]: predicted sigmas for each trajectory. 
+                prediction head.
+                H: number of predicted heads.
+            mu[torch.tensor(B, A, T, H, D)]: predicted means for each trajectory.
+            sigma[torch.tensor(B, A, T, H, D)]: predicted sigmas for each trajectory.
         """
         device = x.device
         B, A, T, D = x.size()
         assert T <= self.encoder_config.T_size, \
             f"Can't forward sequence of length {T}, time block size is {self.encoder_config.T_size}"
-        
+
         # x: (B, A, T, Dx=3) -> (B, A, T, D=embed_size)
-        x = self.agents_fe(x) 
-    
+        x = self.agents_fe(x)
+
         time_pe = torch.arange(0, T, dtype=torch.long, device=device).unsqueeze(0)  # shape (1, T)
         # time emb: (1, 1, T, D=n_embed)
-        time_emb = self.time_pe(time_pe).unsqueeze(dim=0)                       
+        time_emb = self.time_pe(time_pe).unsqueeze(dim=0)
         x = self.drop(x + time_emb)
-        
+
         # c: (B, T, P, Dc=5)
         cx = kwargs.get('context')
         adj = kwargs.get('adjacency')
@@ -160,15 +162,15 @@ class AmeliaTF(nn.Module):
 
         # cx: (B * A, D=embed_size)
         cx = self.context_fe(cx, adj=adj)
-        
+
         # transformer blocks modeling interactions across-time and across-agents
         mask = kwargs.get('mask')
         for block in self.att_blocks:
-            x = block(x, cx, mask=mask) 
+            x = block(x, cx, mask=mask)
 
-        # prediction 
+        # prediction
         x = self.refine_mlp(x)
 
-        # decoding 
+        # decoding
         pred_scores, mu, sigma = self.decoder_head(x)
         return pred_scores, mu, sigma
