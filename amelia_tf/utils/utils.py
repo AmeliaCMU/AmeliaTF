7e85c11390e3894d76ba5592d3d365f43bab55ea
Alonso Cano <45244628+alonsocanov@users.noreply.github.com>
2025-11-06 11:50:58 -0600

    tmp

--- amelia_tf/utils/utils.py
+++ amelia_tf/utils/utils.py
@@ -173,7 +173,7 @@ def plot_scene_batch(
     agents[C.UNKNOWN] = image
 
     if plot_full_scene:
-        tag_i = f"{airport}_scene-_{scene['scenario_id']}_{tag}"
+        tag_i = f"_scene-_{scene['scenario_id']}_{tag}"
         print(f"Plotting scene {tag_i}")
         print(scene['agent_types'].shape, num_agents)
         num_agents = scene['num_agents'][0]
