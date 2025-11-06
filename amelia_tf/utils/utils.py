9c3a62277df19d4bcb7a3929e28c8dfc4cde10c0
Alonso Cano <45244628+alonsocanov@users.noreply.github.com>
2025-11-06 12:19:36 -0600

    tmp

--- amelia_tf/utils/utils.py
+++ amelia_tf/utils/utils.py
@@ -175,7 +175,7 @@ def plot_scene_batch(
     if plot_full_scene:
         tag_i = f"_scene-_{scene['scenario_id']}_{tag}"
         print(f"Plotting scene {tag_i}")
-        print(scene['agent_types'].shape, scene['num_agents'][0])
+        print(scene['agent_types'].shape, scene['num_agents'])
         num_agents = scene['num_agents'][0]
         zipped = zip(
             pred_scores,           # B, N, H
