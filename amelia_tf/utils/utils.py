03019536c04fc5980b81599925d2319183b700cb
Alonso Cano <45244628+alonsocanov@users.noreply.github.com>
2025-11-06 12:50:22 -0600

    test

--- amelia_tf/utils/utils.py
+++ amelia_tf/utils/utils.py
@@ -176,7 +176,8 @@ def plot_scene_batch(
         tag_i = f"_scene-_{scene['scenario_id']}_{tag}"
         print(f"Plotting scene {tag_i}")
         print(scene['agent_types'].shape, scene['num_agents'].shape, scene['num_agents'])
-        num_agents = scene['num_agents'][0]
+        # num_agents = scene['num_agents'][0]
+        num_agents = scene['num_agents'].max().item()
         zipped = zip(
             pred_scores,           # B, N, H
             mus,                   # B, N, T, H, Dxy
