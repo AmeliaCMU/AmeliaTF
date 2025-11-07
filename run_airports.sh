cea6c9727adc8e200105af2a66f50923e540e75a
Alonso Cano <45244628+alonsocanov@users.noreply.github.com>
2025-11-07 12:45:19 -0600

    run all

--- run_airports.sh
+++ run_airports.sh
@@ -1,19 +1,31 @@
-python3 amelia_tf/train.py data=kmdw model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_fix_rot model.optimizer.lr=0.0001 data.dataset.config.min_agents=2
+python3 amelia_tf/train.py data=kmdw model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_run model.optimizer.lr=0.0001 data.dataset.config.min_agents=2
 
-python3 amelia_tf/train.py data=kewr model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_fix_rot model.optimizer.lr=0.0001 data.dataset.config.min_agents=2
+python3 amelia_tf/train.py data=kewr model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_run model.optimizer.lr=0.0001 data.dataset.config.min_agents=2
 
-python3 amelia_tf/train.py data=kbos model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_fix_rot model.optimizer.lr=0.0001 data.dataset.config.min_agents=3
+python3 amelia_tf/train.py data=kbos model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_run model.optimizer.lr=0.0001 data.dataset.config.min_agents=3
 
-python3 amelia_tf/train.py data=ksfo model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_fix_rot model.optimizer.lr=0.00001 data.dataset.config.min_agents=2
+python3 amelia_tf/train.py data=ksfo model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_run model.optimizer.lr=0.00001 data.dataset.config.min_agents=2
 
-python3 amelia_tf/train.py data=ksea model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_fix_rot model.optimizer.lr=0.0001 data.dataset.config.min_agents=2
+python3 amelia_tf/train.py data=ksea model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_run model.optimizer.lr=0.0001 data.dataset.config.min_agents=2
 
-python3 amelia_tf/train.py data=kdca model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_fix_rot model.optimizer.lr=0.00001 data.dataset.config.min_agents=2
+python3 amelia_tf/train.py data=kdca model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_run model.optimizer.lr=0.00001 data.dataset.config.min_agents=2
 
-python3 amelia_tf/train.py data=panc model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_fix_rot model.optimizer.lr=0.00001 data.dataset.config.min_agents=2
+python3 amelia_tf/train.py data=panc model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_run model.optimizer.lr=0.00001 data.dataset.config.min_agents=2
 
-python3 amelia_tf/train.py data=klax model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_fix_rot model.optimizer.lr=0.00001 data.dataset.config.min_agents=2
+python3 amelia_tf/train.py data=klax model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_run model.optimizer.lr=0.00001 data.dataset.config.min_agents=2
 
-python3 amelia_tf/train.py data=kmsy model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_fix_rot model.optimizer.lr=0.0001 data.dataset.config.min_agents=3
+python3 amelia_tf/train.py data=kmsy model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_run model.optimizer.lr=0.0001 data.dataset.config.min_agents=3
 
-python3 amelia_tf/train.py data=kjfk model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_fix_rot model.optimizer.lr=0.0001 data.dataset.config.min_agents=2
+python3 amelia_tf/train.py data=kjfk model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_run model.optimizer.lr=0.0001 data.dataset.config.min_agents=2
+
+python3 amelia_tf/train.py data=seen-1 model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_run model.optimizer.lr=0.0001 data.dataset.config.min_agents=2
+
+python3 amelia_tf/train.py data=seen-2 model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_run model.optimizer.lr=0.0001 data.dataset.config.min_agents=2
+
+python3 amelia_tf/train.py data=seen-3 model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_run model.optimizer.lr=0.0001 data.dataset.config.min_agents=2
+
+python3 amelia_tf/train.py data=seen-4 model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_run model.optimizer.lr=0.0001 data.dataset.config.min_agents=2
+
+python3 amelia_tf/train.py data=seen-7 model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_run model.optimizer.lr=0.0001 data.dataset.config.min_agents=2
+
+python3 amelia_tf/train.py data=seen-all model=marginal trainer=gpu trainer.devices=[1] data.dataset.config.sampling_strategy=critical logger.wandb.project=amelia_run model.optimizer.lr=0.0001 data.dataset.config.min_agents=2
